b08e2de604ecc59711301f57a5ec4196af2d7ef5
Luceva2002 <luceva2002@gmail.com>
2024-12-17 16:58:02 +0100

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,22 +6,51 @@
 /*   By: luevange <luevange@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:19:39 by luevange          #+#    #+#             */
-/*   Updated: 2024/12/16 13:41:44 by luevange         ###   ########.fr       */
+/*   Updated: 2024/12/17 16:56:56 by luevange         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	findchar(char const *str, char c)
+{
+	while(*str)
+	{
+		if (*str == c)
+			return (1);
+		str ++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		j;
-	char	*str;
-
+	int i;
+	int j;
+	int k;
+	char *array;
+	
+	k = 0;
 	i = 0;
-	j = 0;
-	str = malloc(ft_strlen(s1));
+	j = ft_strlen(s1) - 1;
+	while (i < j && findchar(set, s1[i]) == 1)
+		i++;
+	while (j > i && findchar(set, s1[j]) == 1)
+		j--;
+	if (ft_strncmp(s1, set, ft_strlen(s1)) == 0)
+		return (NULL);
+	array = malloc(j - i + 2);
+	while (i < j + 1)
+	{
+		array[k] = s1[i];
+		k	++;
+		i++; 
+	}
+	return (array);
 }
+/* int main (int ac, char **av)
 {
 	printf("%s", ft_strtrim((char const *)av[1], (char const *)av[2]));
+	return (0);
 }
+ */
